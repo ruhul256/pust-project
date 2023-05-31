a648e7bb047b13d8c449ca6d0dfd032a2984488d
Ruhul <ruhulstat6@gmail.com>
2023-05-31 22:29:21 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [image, setImage] = useState(null);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(image);
+    if (image) {
+      axios
+        .post("", image)
+        .then((res) => console.log(res))
+        .catch((err) => console.log(err));
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="file"
+          placeholder="Please provide a image"
+          onChange={(e) => setImage(e.target.files[0])}
+        />
+        <button type="submit">submit</button>
+      </form>
     </div>
   );
 }
